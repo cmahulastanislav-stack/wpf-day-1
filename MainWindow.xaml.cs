806a6a794d48da9d24bc58ad88dd887185f359b1
cmahulastanislav-stack <c.mahula.stanislav@student.uzhnu.edu.ua>
2026-04-10 23:38:52 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace MyWpfApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ResultText.Text = "Ви ввели: " + InputBox.Text;
+        }
+    }
+}
