fb1efe8da882a095d281308961e02b9c2f0728d3
cmahulastanislav-stack <c.mahula.stanislav@student.uzhnu.edu.ua>
2026-04-18 02:25:35 +0300

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using Wpf_day_2.views;
 
-namespace MyWpfApp
+namespace Wpf_day_2
 {
     public partial class MainWindow : Window
     {
+        private bool isDark = false;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -24,6 +27,23 @@ namespace MyWpfApp
         private void Settings_Click(object sender, RoutedEventArgs e)
         {
             MainFrame.Navigate(new SettingsPage());
+        }
+
+        private void SwitchTheme_Click(object sender, RoutedEventArgs e)
+        {
+            var dict = new ResourceDictionary();
+
+            if (isDark)
+                dict.Source = new Uri("Resources/LightTheme.xaml", UriKind.Relative);
+            else
+                dict.Source = new Uri("Resources/DarkTheme.xaml", UriKind.Relative);
+
+            Application.Current.Resources.MergedDictionaries.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(dict);
+            Application.Current.Resources.MergedDictionaries.Add(
+                new ResourceDictionary { Source = new Uri("Resources/Styles.xaml", UriKind.Relative) });
+
+            isDark = !isDark;
         }
     }
 }
